04a86583dec61975e42b1fc40bb47c7b6ae3ec9c
40891 <40891@longroad.ac.uk>
2014-09-11 16:08:08 +0100

    Revert "Program to add 4 numbers and then give a total"
    This reverts commit 152405e6fc8c6254ed65a8e01424080a01b6f91c.

--- add 4 numbers.py
+++
@@ -1,7 +0,0 @@
-print("this program will add together 4 integers for you :)")
-num1=float(input("please enter your first number"))
-num2=float(input("please enter your second number"))
-num3=float(input("please enter your third number"))
-num4=float(input("please enter your final number"))
-total_num=num1+num2+num3+num4
-print("this is your total value:{0}".format(total_num))

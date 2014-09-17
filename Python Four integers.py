ffc70b4834f6d5d94afbbf3f5f37518f3138f864
40891 <40891@longroad.ac.uk>
2014-09-17 09:00:03 +0100

    Add together 4 numbers

---
+++ Python Four integers.py
@@ -0,0 +1,8 @@
+print("This program will add together 3 numbers")
+number_1=int(input("write first number"))
+number_2=int(input("write second number"))
+number_3=int(input("write third number"))
+number_4=int(input("wirte fourth number"))
+total_number=number_1+number_2+number_3+number_4
+print("here is the total value:{0}".format(total_number))
+
